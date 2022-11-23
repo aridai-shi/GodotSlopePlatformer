21fd1226658bd43a711873acffde48c79c1430f9
aridai <jakubkaczma5@gmail.com>
2022-11-23 20:51:54 +0100

    Rewrote the Player code to work on a limited FSM implementation

--- Player.gd
+++ Player.gd
@@ -1,127 +1,126 @@
 extends KinematicBody2D
 
-var run_speed = 9.5 # acceleration
-var jump_speed = -500
+var runSpeed = 9.5 # acceleration
+var machSpeed = 0
+var speedCap = 1200
+var groundFriction = 0.99
+var airFriction = 0.995
+var frictionMultA = 0.98
+var frictionMultB = 0.96
+var jumpSpeed = -400
 var gravity = 800
-var velocity = Vector2()
-var jumping = false # in charge of the jumping ""state"" and animations
-var dir = true # in charge of animation direection
-var speed_cap = 1200
-var gravity_speed = 575
+var globalVelocity = Vector2()
+var localVelocity = Vector2()
 var snap = Vector2.ZERO # snap vector
-var stoppingRunning = false; # purely in charge of switching to the idle animation sooner - to be ignored
 var lastNormal = Vector2.ZERO
 var leftFloor = false
-var secondaryGravity = 0
+var machGravity = 0
+var jumpAnim = false
+enum states {
+	GROUND,
+	AIR,
+	G_MACH
+}
+var state : int = states.GROUND
 
-func get_input(delta):
-	var horiz =  Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left") # simplified left-right check
-	var jump = Input.is_action_just_pressed('ui_up')
-	var jumpnt = Input.is_action_just_released('ui_up') #for variable jump height
-	if is_on_floor():
-		$CoyoteTimer.start(0.15)
-		leftFloor = false
-	elif !leftFloor && !jumping:
-		leftFloor = true
-		velocity.x = velocity.rotated(rotation).x # jump off at an angle, no matter the rotation
-	if jump:
-		$JumpBufferTimer.start(0.15)
-	if jumpnt && velocity.y < 0:
-		velocity.y = velocity.y*0.55 # half-measure for global jump height
-	if !jumping and $JumpBufferTimer.time_left > 0 and $CoyoteTimer.time_left > 0: # if we hit jump, aren't off the ground yet and weren't in a jumping state before
-		snap = Vector2.ZERO
-		jumping = true # enable jumping state
-		$ControlLockTimer.start(0.15)
-		var addForce = Vector2(0,jump_speed) # after we rotate, keep the pre-rotation direction
-		var tempVel = velocity+addForce # add force from the jump
-		velocity = tempVel.rotated(rotation)
-		rotation = 0 # rotate upright
-	stoppingRunning = (horiz == 0) # if we're not pressing anything, make the idle animation appear sooner
-	if velocity.x!=0: # if we're moving horizontally
-		if horiz==0: # but not pressing any direction
-			velocity.x = velocity.x*0.975 # apply medium friction value
-			if abs(velocity.x)<100 && abs(rotation)<(PI/10): # if we're supposed to be almost still
-				velocity.x=velocity.x*0.8 # apply extra friction
-				stoppingRunning = false # let the running animation start up quickly again
-		elif horiz!=velocity.x/abs(velocity.x): # if we're pressing the opposite direction to the one we're moving at
-			velocity.x = velocity.x*0.95 # apply high friction
-		else:
-			velocity.x = velocity.x*0.99 # apply low friction so the player doesn't rocket off
-	if $ControlLockTimer.time_left <= 0:
-		velocity.x+=horiz*run_speed
-	velocity.x = clamp(velocity.x,-speed_cap,speed_cap)
-	animate()
-	
+
+var horiz
+var jump
+var jumpnt
+
+func get_input():
+	horiz =  Input.get_axis("ui_left","ui_right") # simplified left-right check
+	jump = Input.is_action_just_pressed('ui_up')
+	jumpnt = Input.is_action_just_released('ui_up') #for variable jump height
+
+func state_process():
+	match state:
+		states.GROUND, states.G_MACH:
+			if (jump):
+				$JumpBufferTimer.start(0.15)
+			if (!is_on_floor()):
+				state = states.AIR
+			else:
+				$CoyoteTimer.start(0.15)
+			if $JumpBufferTimer.time_left > 0 and $CoyoteTimer.time_left > 0:
+				localVelocity += Vector2(0,jumpSpeed).rotated(-rotation)
+				jumpAnim = true
+				snap = Vector2.ZERO
+			horizontal_friction(groundFriction)
+			continue;
+		states.GROUND:
+			if (abs(localVelocity.x)>=machSpeed):
+				state = states.G_MACH
+		states.G_MACH:
+			if (abs(localVelocity.x)<machSpeed):
+				state = states.GROUND
+		states.AIR:
+			if (is_on_floor()):
+				state = states.GROUND
+				jumpAnim = false
+			horizontal_friction(airFriction)
+		_:
+			pass
+
+
+func horizontal_friction(friction):
+	localVelocity.x = localVelocity.x*friction
+	if horiz==0: 
+		localVelocity.x = localVelocity.x*frictionMultA 
+		if abs(localVelocity.x)<100: 
+			localVelocity.x=localVelocity.x*frictionMultB 
+	elif horiz!=localVelocity.x/abs(localVelocity.x): 
+		localVelocity.x = localVelocity.x*frictionMultB
+	localVelocity.x+=horiz*runSpeed
+	localVelocity.x = clamp(localVelocity.x,-speedCap,speedCap)
+
 func animate():
 	$AnimatedSprite.speed_scale = 1
-	if(!jumping):
-		if (abs(velocity.x)>20 && !stoppingRunning) || abs(velocity.x)>100 :
-			$AnimatedSprite.flip_h = true if velocity.x/abs(velocity.x)==-1 else false # changes direction if we're moving at speeds that go beyond the idle animation
-			if abs(velocity.x)<(gravity_speed-50):
+	if jumpAnim:
+		$AnimatedSprite.animation = "ball"
+		return
+	if (abs(localVelocity.x)>20):
+		$AnimatedSprite.flip_h = true if localVelocity.x/abs(localVelocity.x)==-1 else false
+	match state:
+		states.GROUND:
+			if (abs(localVelocity.x)<20):
+				$AnimatedSprite.animation = "idle"
+			elif abs(localVelocity.x)<(machSpeed-50):
 				$AnimatedSprite.animation = "walk"
-				$AnimatedSprite.speed_scale = 1.5*abs(velocity.x)/(gravity_speed-50) # walk animation goes faster the faster you go until you start running
-			elif abs(velocity.x)>1000:
-				$AnimatedSprite.animation = "mach"
+				$AnimatedSprite.speed_scale = 1.5*abs(localVelocity.x)/(machSpeed-50) # walk animation goes faster the faster you go until you start running
 			else:
 				$AnimatedSprite.animation = "run"
-		else:
-			$AnimatedSprite.animation = "idle"
-	else:
-		$AnimatedSprite.animation = "ball" # the ground animations are kept if you run off a slope instead of jumping off it voluntarily
-
-func angle_dif(from : float, to : float):
-	var ans = fposmod(to - from, TAU)
-	if ans > PI:
-		ans -= TAU
-	return ans
+		states.AIR:
+			pass
+		states.G_MACH:
+			$AnimatedSprite.animation = "mach"
+		_:
+			pass
 
 func _physics_process(delta):
-	get_input(delta)
-	secondaryGravity =  gravity 
-	var flipped = false
-	if is_on_floor():
-		if try_vel(delta).length()<1:
-			secondaryGravity = 0
-	if gravity_off():
-		if !jumping:
-			secondaryGravity = 0
-			velocity.y+=40
-	if jumping and is_on_floor() and $JumpBufferTimer.time_left <= 0 and $ControlLockTimer.time_left <= 0:
-		jumping = false
-	var floorAngleWrapped = getShortestFloorCast().get_collision_normal().angle() + PI/2
-	floorAngleWrapped = wrapf(floorAngleWrapped,-PI,PI)
-	var rotationWrapped = rotation
-	rotationWrapped = wrapf(rotationWrapped,-PI,PI)
-	var deltaAngle = abs(rotationWrapped-floorAngleWrapped)
-	if get_tree().get_frame()%10==0:
-		var debugText = ""
-		debugText += "FLOOR NORMAL ANGLE: " +str(rad2deg(floorAngleWrapped))
-		debugText += "\nROTATION ANGLE: " +str(rad2deg(rotationWrapped))
-		debugText += "\nDELTA ANGLE: " +str(rad2deg(deltaAngle))
-		$CanvasLayer/DebugLabel.text = debugText 
-	if is_on_floor() and !jumping:
+	get_input()
+	state_process()
+	localVelocity.x = localVelocity.x if abs(localVelocity.x)>3 else 0
+	snap = global_transform.y * 75 if (-localVelocity.y<gravity*delta || gravity_off()) else Vector2.ZERO
+	if state!=states.AIR:
 		rotation = getShortestFloorCast().get_collision_normal().angle() + PI/2 # align with floor when we're on it
 	else:
-		rotation = 0 # stay upright when in midair
-	if Input.is_action_pressed("ui_down"):
-		breakpoint
-	velocity.x = velocity.x if abs(velocity.x)>3 else 0 # removes fractional x velocities that just cause the player to slide when idle
-	snap = global_transform.y * 75 if ((!jumping && -velocity.y<secondaryGravity*delta) || (!jumping && gravity_off())) else Vector2.ZERO
-	var tempNewVel = velocity.rotated(rotation)+Vector2(0,secondaryGravity *delta)
-	velocity = move_and_slide(tempNewVel, -transform.y, true) # adding gravity after rotating velocity in order to make it global and factor it into the speed of uphill movement
-	velocity = velocity.rotated(-rotation) # converts velocity back to local after m_a_s_w_s() rotates it
-	
+		rotation = 0 
+	globalVelocity = localVelocity.rotated(rotation)
+	if state != states.G_MACH:
+		globalVelocity+=Vector2(0,gravity*delta)
+	else:
+		localVelocity.y+=40
+		globalVelocity = localVelocity.rotated(rotation)
+	globalVelocity = move_and_slide_with_snap(globalVelocity,snap,-global_transform.y)
+	localVelocity = globalVelocity.rotated(-rotation)
+	animate()
+
 func is_on_floor():
 	return getShortestFloorCast().is_colliding() # custom is_on_floor() detection cause the official one doesn't work very well here
 
 func gravity_off():
-	#
-	return abs(velocity.x)>gravity_speed && abs(rotation_degrees)>60
-
-func try_vel(delta):
-	var diff = move_and_slide_with_snap(velocity.rotated(rotation)+Vector2(0,gravity *delta),snap, -transform.y, true) # move
-	move_and_slide_with_snap(-velocity.rotated(rotation)-Vector2(0,gravity *delta),snap, -transform.y) # undo the move in the exact same frame
-	return diff # how much did we move?
+	return abs(localVelocity.x)>machSpeed && abs(rotation_degrees)>60
 
 func getShortestFloorCast():
 	if ((global_position-$FloorCast1.get_collision_point()).length()>(global_position-$FloorCast2.get_collision_point()).length()):
